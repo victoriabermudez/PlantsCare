373aa8d8a6fb61e3b85d99958e607dfeef0e00fb
Victoria Bermudez <vmbermudez044@gmail.com>
2022-01-04 08:07:49 -0500

    Initial Commit

---
+++ PlantsCare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlantsCare
+//
+//  Created by Victoria Bermudez (student LM) on 1/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlantsCare/PlantsCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsCareApp.swift
+//  PlantsCare
+//
+//  Created by Victoria Bermudez (student LM) on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PlantsCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
