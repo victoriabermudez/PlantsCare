47fa14c1886816535db76d16171894bff16c5e62
Katie Fang <katherine.j.fang@gmail.com>
2022-01-24 08:57:08 -0500

    added comments to CropDetail and ContentView.

--- PlantsCare/ContentView.swift
+++ PlantsCare/ContentView.swift
@@ -13,27 +13,25 @@ struct ContentView: View {
     @State var res = Result()
     
     var body: some View {
-        ZStack{
-            Image("Background")
-                .resizable()
-                .edgesIgnoringSafeArea(.all)
-            VStack{
-                Text("Rick & MOREty")
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                NavigationView{
-                    
-                    List(fetchData.responses.results){result in
+        ZStack{ //start of ZStack
+            Image("Background") //adds the image behind the list, creating a banner effect
+                .resizable() //scales the image to the size of the screen
+                .edgesIgnoringSafeArea(.all) //extends beyond the safe area
+            VStack{ //start of VStack
+                Text("Rick & MOREty") //adds text (title of app)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/) //makes the text bolded
+                NavigationView { //start of NavigationView
+                    List(fetchData.responses.results){ result in //list
                         NavigationLink(
                             destination: CropDetail(res : result),
                             label: {
                                 HStack{
-                                    //Text("hello")
-                                    KFImage(/*URL(string: */result.image)
+                                    KFImage(/*URL(string: */result.image) //adds the image from the API to the side of the list
                                         .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 100, height: 50)
-                                    Text(result.name ?? "no name")
-                                        .fontWeight(.bold)
+                                        .aspectRatio(contentMode: .fit) //makes the aspect ratio fit
+                                        .frame(width: 100, height: 50) //sets the width and height of the image
+                                    Text(result.name ?? "no name") //gets the name of the character from the API
+                                        .fontWeight(.bold) //makes this text bold
                                         .foregroundColor(Color.black) //changes the color of the text to black
                                         .multilineTextAlignment(.center) //aligns the text to center
                                         .shadow(color: .green, radius: 0.5, x: 0, y: 2) //adds a dropshadow to the text
@@ -47,6 +45,7 @@ struct ContentView: View {
                     }
                     .navigationBarTitle("")
                             .navigationBarHidden(true)
+                    //hid the NavigationBarTitle so that the already established banner would be closer to the list
                 }
             }
         }
--- PlantsCare/CropDetail.swift
+++ PlantsCare/CropDetail.swift
@@ -14,74 +14,74 @@ struct CropDetail: View {
     
     var body: some View {
         ZStack{
-            Image("Background")
+            Image("Background") //adds a background to the more detailed pages
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
             
-            VStack{
-                KFImage(/*URL(string: */res.image)
+            VStack{ //start of VStack
+                KFImage(/*URL(string: */res.image) //adds the image from the API of the character to their respective pages
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
-                HStack {
-                    Text("Name: ")
-                        .fontWeight(.semibold)
+                    .frame(width: 200, height: 200) //establishes a width and height for the image
+                HStack { //start of HStack
+                    Text("Name: ") //adds text
+                        .fontWeight(.semibold) //makes the font semibold
                         .foregroundColor(Color.black) //changes the color of the text to black
                         .multilineTextAlignment(.center) //aligns the text to center
                         .shadow(color: .green, radius: 0.5, x: 0, y: 2) //adds a dropshadow to the text
                         .shadow(color: .blue, radius: 0.5, x: 0, y: 2.1) //adds a dropshadow to the text
                         .font(.system(.title, design: .rounded)) //makes the font have rounded edges
-                    Text(res.name ?? "NA")
-                        .foregroundColor(Color.purple)
+                    Text(res.name ?? "NA") //gets the character's name from the API and displays the text
+                        .foregroundColor(Color.purple) //makes the text purple
                         .fontWeight(.bold)//makes this text have a purple color
                 }
                 HStack {
-                    Text("Status: ")
-                        .fontWeight(.semibold)
+                    Text("Status: ") //adds text
+                        .fontWeight(.semibold) //makes the font semibold
                         .foregroundColor(Color.black) //changes the color of the text to black
                         .multilineTextAlignment(.center) //aligns the text to center
                         .shadow(color: .green, radius: 0.5, x: 0, y: 2) //adds a dropshadow to the text
                         .shadow(color: .blue, radius: 0.5, x: 0, y: 2.1) //adds a dropshadow to the text
                         .font(.system(.title, design: .rounded)) //rounds font
-                    Text(res.status ?? "NA")
-                        .foregroundColor(Color.purple)
-                        .fontWeight(.bold)
+                    Text(res.status ?? "NA") //gets the character's status from the API and displays the text
+                        .foregroundColor(Color.purple) //makes the text purple
+                        .fontWeight(.bold)//makes this text have a purple color
                 }
                 HStack {
-                    Text("Species: ")
-                        .fontWeight(.semibold)
+                    Text("Species: ") //adds text
+                        .fontWeight(.semibold) //makes the font semibold
                         .foregroundColor(Color.black) //changes the color of the text to black
                         .multilineTextAlignment(.center) //aligns the text to center
                         .shadow(color: .green, radius: 0.5, x: 0, y: 2) //adds a dropshadow to the text
                         .shadow(color: .blue, radius: 0.5, x: 0, y: 2.1) //adds a dropshadow to the text
                         .font(.system(.title, design: .rounded)) //rounds font
-                    Text(res.species ?? "NA")
-                        .foregroundColor(Color.purple)
-                        .fontWeight(.bold)
+                    Text(res.species ?? "NA") //gets the character's species from the API and displays the text
+                        .foregroundColor(Color.purple) //makes the text purple
+                        .fontWeight(.bold)//makes this text have a purple color
                 }
                 HStack {
-                    Text("Gender: ")
-                        .fontWeight(.semibold)
+                    Text("Gender: ") //adds text
+                        .fontWeight(.semibold) //makes the font semibold
                         .foregroundColor(Color.black) //changes the color of the text to black
                         .multilineTextAlignment(.center) //aligns the text to center
                         .shadow(color: .green, radius: 0.5, x: 0, y: 2) //adds a dropshadow to the text
                         .shadow(color: .blue, radius: 0.5, x: 0, y: 2.1) //adds a dropshadow to the text
                         .font(.system(.title, design: .rounded)) //rounds font
-                    Text(res.gender ?? "NA")
-                        .foregroundColor(Color.purple)
-                        .fontWeight(.bold)
+                    Text(res.gender ?? "NA") //gets the character's gender from the API and displays the text
+                        .foregroundColor(Color.purple) //makes the text purple
+                        .fontWeight(.bold)//makes this text have a purple color
                 }
                 HStack {
-                    Text("Origin: ")
-                        .fontWeight(.semibold)
+                    Text("Origin: ") //adds text
+                        .fontWeight(.semibold) //makes the font semibold
                         .foregroundColor(Color.black) //changes the color of the text to black
                         .multilineTextAlignment(.center) //aligns the text to center
                         .shadow(color: .green, radius: 0.5, x: 0, y: 2) //adds a dropshadow to the text
                         .shadow(color: .blue, radius: 0.5, x: 0, y: 2.1) //adds a dropshadow to the text
                         .font(.system(.title, design: .rounded)) //rounds font
-                    Text(res.origin.name ?? "NA")
-                        .foregroundColor(Color.purple)
-                        .fontWeight(.bold)
+                    Text(res.origin.name ?? "NA") //gets the character's origin from the API and displays the text
+                        .foregroundColor(Color.purple) //makes the text purple
+                        .fontWeight(.bold)//makes this text have a purple color
                 }
             }
         }
